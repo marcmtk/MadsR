2116c07c14e504b3e351630374d278b812eb1b05
marcmtk <marc.mtk@gmail.com>
2016-02-16 21:57:06 +0100

    Minor changes to testing of read_mads

--- tests/testthat/test_read_mads.R
+++ tests/testthat/test_read_mads.R
@@ -1,8 +1,9 @@
+context("read_mads")
 test_that("Non-empty Data frame from read_mads", {
 
-  df <- read_mads("./data/MADS-like.csv", "analyser")
+  df <- read_mads("MADS-like.csv", "analyser")
 
   expect_is(df, "data.frame")
-  expect_gt(length(df)), 0)
+  expect_gt(length(df), 0)
   expect_gt(nrow(df), 0)
 })
